b54727fe23b40f28228bbf5b8b612addb4c16d73
topskydev55 <seniordev55@gmail.com>
2024-12-21 13:17:54 -0600

    feat(zebra): create simple zebra project

---
+++ App.tsx
@@ -0,0 +1,172 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useCallback, useEffect, useState} from 'react';
+import debounce from 'lodash/debounce';
+
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {
+  connectToDevice,
+  getAllDevices,
+  ZebraEvent,
+  ZebraEventEmitter,
+  type ZebraResultPayload,
+  type ZebraRfidResultPayload,
+} from 'react-native-zebra-rfid-barcode-ef-tech';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: 'white',
+  },
+  partial: {
+    flex: 1,
+  },
+  item: {
+    height: 50,
+    paddingHorizontal: 15,
+    justifyContent: 'center',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#ccc',
+  },
+  text: {
+    color: '#333',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginVertical: 5,
+  },
+});
+
+
+function App(): React.JSX.Element {
+  const [listDevices, setListDevices] = useState<string[]>([]);
+  const [listBarcodes, setListBarcodes] = useState<string[]>([]);
+  const [listRfid, setListRfid] = useState<string[]>([]);
+
+  useEffect(() => {
+    getListRfidDevices();
+
+    const barcodeEvent = ZebraEventEmitter.addListener(
+      ZebraEvent.ON_BARCODE,
+      (e: ZebraResultPayload) => {
+        handleBarcodeEvent(e.data);
+      },
+    );
+
+    const rfidEvent = ZebraEventEmitter.addListener(
+      ZebraEvent.ON_RFID,
+      (e: ZebraRfidResultPayload) => {
+        handleRfidEvent(e.data);
+      },
+    );
+
+    const deviceConnectEvent = ZebraEventEmitter.addListener(
+      ZebraEvent.ON_DEVICE_CONNECTED,
+      (e: ZebraResultPayload) => {
+        console.log(e.data); // "Connect successfully" || "Connect failed"
+      },
+    );
+
+    return () => {
+      barcodeEvent.remove();
+      rfidEvent.remove();
+      deviceConnectEvent.remove();
+    };
+  }, []);
+
+  const handleRfidEvent = useCallback(
+    debounce((newData: string[]) => {
+      setListRfid(pre => [...pre, ...newData]);
+    }, 200),
+    [],
+  );
+
+  const handleBarcodeEvent = useCallback(
+    debounce((newData: string) => {
+      setListBarcodes(pre => [...pre, newData]);
+    }, 200),
+    [],
+  );
+
+  const getListRfidDevices = async () => {
+    const listDevices = await getAllDevices();
+    setListDevices(listDevices);
+  };
+
+  return (
+    <View style={styles.container}>
+    <View
+      style={{
+        maxHeight: 200,
+      }}
+    >
+      <Text style={[styles.text, styles.title]}>
+        Devices: {listDevices.length}
+      </Text>
+      <FlatList
+        style={{ backgroundColor: '#FEF3C7' }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        data={listDevices}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => connectToDevice(item)}
+            style={styles.item}
+          >
+            <Text style={styles.text}>{item}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+
+    <View style={styles.partial}>
+      <Text style={[styles.text, styles.title]}>
+        Barcodes: {listBarcodes.length}
+      </Text>
+      <FlatList
+        style={{ backgroundColor: '#DCFCE7' }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        data={listBarcodes}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+    </View>
+
+    <View style={styles.partial}>
+      <Text style={[styles.text, styles.title]}>
+        RFIDs: {listRfid.length}
+      </Text>
+      <FlatList
+        style={{ backgroundColor: '#E0F2FE', marginBottom: 10 }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        data={listRfid}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+    </View>
+  </View>
+  );
+}
+
+
+export default App;
---
+++ android/app/src/main/java/com/zebra/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.zebra
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      requestBluetoothPermissions();
+    }
+  }
+
+  @RequiresApi(Build.VERSION_CODES.S)
+  private fun requestBluetoothPermissions() {
+    val permissions = arrayOf(
+      Manifest.permission.BLUETOOTH_CONNECT,
+      Manifest.permission.BLUETOOTH_SCAN,
+      Manifest.permission.BLUETOOTH_ADVERTISE
+    )
+
+    val permissionToRequest = permissions.filter {
+      ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+    }
+
+    if (permissionToRequest.isNotEmpty()) {
+      ActivityCompat.requestPermissions(
+        this,
+        permissionToRequest.toTypedArray(),
+        1
+      )
+    }
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "zebra"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
